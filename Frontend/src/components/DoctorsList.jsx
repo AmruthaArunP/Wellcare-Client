e4d59b00be58f09a0a6da83c77957258c1366d88
amrutha <pamruthaarun@gmail.com>
2024-04-09 22:50:06 +0530

    image path updated in doctor list using env var

--- Frontend/src/components/DoctorsList.jsx
+++ Frontend/src/components/DoctorsList.jsx
@@ -34,7 +34,7 @@ function DoctorsList({docData}) {
         {docData.length > 0 ? (
         docData.map((doctor, index) => (
           <div key={index} className="bg-white p-4 rounded-lg shadow-md">
-            <img src={`${import.meta.env.VITE_BASE_URL}/images/${doctor.image}`} alt={doctor.name} className="w-full h-56 object-cover mb-4 rounded-md"  />
+            <img src={`${import.meta.env.VITE_BASE_URL}images/${doctor.image}`} alt={doctor.name} className="w-full h-56 object-cover mb-4 rounded-md"  />
             <h2 className="text-lg font-semibold">Dr.{doctor.name}</h2>
             <p className="text-sm text-gray-600 mb-2">{doctor.qualification}</p>
             <p className="text-sm text-gray-600 mb-2">Rs.{doctor.fee}</p>
